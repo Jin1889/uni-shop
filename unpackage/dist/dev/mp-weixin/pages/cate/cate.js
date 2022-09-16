eb065e0273f981446bfef43ce1684dbd81cab0fe
Jin1889 <djwang1889@163.com>
2022-09-16 15:00:16 +0800

    完成商品详情功能

--- unpackage/dist/dev/mp-weixin/pages/cate/cate.js
+++ unpackage/dist/dev/mp-weixin/pages/cate/cate.js
@@ -98,7 +98,7 @@ var components
 try {
   components = {
     mySearch: function() {
-      return __webpack_require__.e(/*! import() | uni_modules/my-search/components/my-search/my-search */ "uni_modules/my-search/components/my-search/my-search").then(__webpack_require__.bind(null, /*! @/uni_modules/my-search/components/my-search/my-search.vue */ 64))
+      return __webpack_require__.e(/*! import() | uni_modules/my-search/components/my-search/my-search */ "uni_modules/my-search/components/my-search/my-search").then(__webpack_require__.bind(null, /*! @/uni_modules/my-search/components/my-search/my-search.vue */ 66))
     }
   }
 } catch (e) {
